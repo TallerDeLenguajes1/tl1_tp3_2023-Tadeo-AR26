9e987704493a498bff90cddccc7f9955740e1dc1
Tadeo Alonso Ruiz <alonsoruiztadeo26@gmail.com>
2023-04-04 17:49:52 -0300

    Ejercicio 04

--- tp3_3.c
+++ tp3_3.c
@@ -21,6 +21,8 @@ struct Cliente{
 }typedef Cliente;
 
 void cargarClientes(Cliente *Lista, int cantClientes);
+void mostrarClientes(Cliente *Lista, int cantClientes);
+float montoAPagar(Producto *Producto);
 
 
 int main(){
@@ -34,7 +36,10 @@ int main(){
 
     Lista = malloc(sizeof(Cliente)*cantClientes);
     cargarClientes(Lista, cantClientes);
-
+    mostrarClientes(Lista, cantClientes);
+    getchar();
+    getchar();
+    free(Lista);
     return 0;
 }
 
@@ -55,13 +60,36 @@ void cargarClientes(Cliente *Lista, int cantClientes){
         Lista->cantidadProductosAPedir = rand()%5+1;
         Lista->Productos = malloc(sizeof(Producto)*Lista->cantidadProductosAPedir);
         for(j = 0; j < Lista->cantidadProductosAPedir; j++){
-            Lista->Productos->ProductoID = j+1;
-            Lista->Productos->cantidad = rand()%10+1;
+            Lista->Productos[j].ProductoID = j+1;
+            Lista->Productos[j].cantidad = rand()%10+1;
             tipoProductoAux = rand()%5;
-            Lista->Productos->tipoProducto = tiposProductos[tipoProductoAux];
-            Lista->Productos->PrecioUnitario = (float)(rand()%100+1);
-            Lista->Productos++;
+            Lista->Productos[j].tipoProducto = malloc(sizeof(char)*strlen(tiposProductos[tipoProductoAux])+1);
+            strcpy(Lista->Productos[j].tipoProducto, tiposProductos[tipoProductoAux]);
+            Lista->Productos[j].PrecioUnitario = (float)(rand()%100+1);
         }
+        Lista++;
+    }
+}
+
+float montoAPagar(Producto *Producto){
+    return((Producto->cantidad)*(Producto->PrecioUnitario));
+}
+
+void mostrarClientes(Cliente *Lista, int cantClientes){
+    int i, j;
+    float montoTotal;
+    for(i = 0; i<cantClientes; i++){
+        printf("Cliente ID: %d\n", Lista->ClienteID);
+        printf("Nombre: %s\n", Lista->NombreCliente);
+        printf("Cantidad de productos solicitados: %d\n", Lista->cantidadProductosAPedir);
+        for(j = 0; j<Lista->cantidadProductosAPedir; j++){
+            printf("    Producto ID: %d\n", Lista->Productos[j].ProductoID);
+            printf("        Cantidad: %d\n", Lista->Productos[j].cantidad);
+            printf("        Tipo de Producto: %s\n", Lista->Productos[j].tipoProducto);
+            printf("        Precio Unitario: %4.2f\n", Lista->Productos[j].PrecioUnitario);
+            montoTotal = montoTotal + montoAPagar(Lista->Productos);
+        }
+        printf("Monto total a pagar: %6.2f\n", montoTotal);
         Lista++;
     }
 }
